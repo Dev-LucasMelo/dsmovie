2281cddba851ff137b447f0516407a6a23731e1e
Dev-LucasMelo <lmelo072002@gmail.com>
2022-01-16 20:23:25 -0300

    First request

--- frontend/src/pages/Listing/index.jsx
+++ frontend/src/pages/Listing/index.jsx
@@ -1,9 +1,18 @@
 import Pagination from "../../components/Pagination"
 import MovieCard from "../../components/MovieCard"
-
+import Axios from "axios"
+import { BASE_URL } from "../../utils/requests"
 
 
 function Listing() {
+
+//FORMA ERRADA (TESTE)
+
+    Axios.get(`${BASE_URL}/movies?size=12&page=0`)
+    .then(response => {
+        console.log(response.data)
+    })   
+
     return (
         <>
             <Pagination />
